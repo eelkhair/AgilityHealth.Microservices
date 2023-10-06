fc6b804bb5f4461e1dca301f553591c7846ec34a
Elkhair <elkhair.elkhair@gmail.com>
2023-10-05 19:16:34 -0500

    Company Orchestration

--- AH.Microservices.Metadata/Tests/Tests.Unit.Application/Setup/SetupTests.cs
+++ AH.Microservices.Metadata/Tests/Tests.Unit.Application/Setup/SetupTests.cs
@@ -6,7 +6,7 @@ public class SetupTests
     public void DatabaseTest()
     {
         var context = DbContextFactory.Create();
-        
+        Assert.IsTrue(context.Countries.All(y=>y.UId != Guid.Empty));
         Assert.IsNotNull(context);
     }
 }
