82e52a36a83029a45cefcc1ac98458483baf6ce3
Elkhair <elkhair.elkhair@gmail.com>
2023-11-05 14:25:20 -0600

    cleanup

--- start-company.sh
+++
@@ -1,5 +0,0 @@
-﻿#!/bin/bash
-cd AH.Microservices.Company/Src/Presentation/AH.Company.Api || exit
-dapr run --app-id company --app-port 5001  dotnet run --resources-path ../../../../Components 
-
-
--- start-metadata.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd AH.Microservices.Metadata/Src/Presentation/AH.Metadata.Api || exit
-dapr run --app-id metadata --app-port 5001  dotnet run --resources-path ../../../../Components 
-
-
