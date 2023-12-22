fc36ac22dc529ef1fd84ff3b39b2c61040c16232
Elkhair <elkhair.elkhair@gmail.com>
2023-12-22 14:25:15 -0600

    change version of blazor

---
+++ FrontEnd/AH.Web/AH.Web/Program.cs
@@ -0,0 +1,29 @@
+using AH.Web.Client.Pages;
+using AH.Web.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Counter).Assembly);
+
+app.Run();
